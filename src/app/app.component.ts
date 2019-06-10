3c52211d94d97f12fde5c31ca44f54d49cbfafba
Maxim Markov <m.markov@itransition.com>
2019-06-10 18:45:04 +0300

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'TMS';
+  title = 'Task management system'
+  taskList = [
+    {
+      name: 'To do',
+      tasks: [
+        {
+          title: 'task 1',
+          author: 'Maxim'
+        },
+        {
+          title: 'task 2',
+          author: 'Alex'
+        }
+      ]
+    },
+    {
+      name: 'Working',
+      tasks: [
+        {
+          title: 'task 3',
+          author: 'Vasya'
+        },
+        {
+          title: 'task 4',
+          author: 'Maxim'
+        }
+      ]
+    },
+    {
+      name: 'Done',
+      tasks: [
+        {
+          title: 'task 5',
+          author: 'Petya'
+        },
+        {
+          title: 'task 6',
+          author: 'Maxim'
+        }
+      ]
+    },
+
+  ]
 }
