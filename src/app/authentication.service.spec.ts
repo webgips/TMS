25ed7faedda7ffe3296d0949a83363d733765978
Maxim Markov <m.markov@itransition.com>
2019-06-19 18:19:47 +0300

    add auth service

---
+++ src/app/authentication.service.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+
+import { AuthenticationService } from './authentication.service';
+import { HttpClientModule } from '@angular/common/http';
+
+describe('AuthenticationService', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientModule ],
+    providers: [
+      AuthenticationService,
+    ]
+  }));
+
+  it('should be created', () => {
+    const service: AuthenticationService = TestBed.get(AuthenticationService);
+    expect(service).toBeTruthy();
+  });
+});
