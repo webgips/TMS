c3c75a04df0ebf1b8dfd9b59b8ab0525b5028571
Maxim Markov <m.markov@itransition.com>
2019-06-20 15:12:32 +0300

    update header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService, User } from '../authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class HeaderComponent {
+  user: User;
+  constructor(private authenticationService: AuthenticationService, private router: Router) {
+    this.authenticationService.currentUser.subscribe(x => this.user = x);
   }
-
+  logout() {
+    this.authenticationService.logout()
+    this.router.navigate(['/login']);
+  }
 }
