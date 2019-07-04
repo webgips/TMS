26eb64fd7f5666c8b0a8acd23fd1f37aa34d8950
Maxim Markov <m.markov@itransition.com>
2019-07-04 16:49:24 +0300

    add board name validate

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { ModalService } from '../services/modal.service';
 import IBoard from '../models/IBoard';
 import { NotificationService } from '../services/notification.service';
 import { MatDialog } from '@angular/material/dialog';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -17,7 +18,10 @@ export class HomeComponent implements OnInit {
   boards: IBoard[] = [];
   private currentBoard: string = localStorage.getItem('currentBoard') ?
                           localStorage.getItem('currentBoard') : null;
-  private newBoard = '';
+  // private newBoard = '';
+  private newBoardForm: FormGroup = new FormGroup({
+    newBoard: new FormControl('', { validators: [Validators.required, this.newBoardValidator.bind(this)], updateOn: 'blur'}),
+  });
   constructor(
     private taskListService: TaskListService,
     private modalService: ModalService,
@@ -27,6 +31,17 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this.taskListService.getBoards().subscribe(boards => this.boards = boards);
   }
+  newBoardValidator(control: FormControl): {[ s: string]: boolean} {
+    const boardsName = [];
+    this.boards.forEach((board, index) => {
+     boardsName.push(board.name);
+    });
+    console.log(boardsName.includes(control.value))
+    if (boardsName.includes(control.value)) {
+      return {'newBoard': true};
+    }
+    return null;
+  }
   showNewBoardModal(e: Event) {
     this.modalService.open('new-board-modal');
   }
@@ -35,8 +50,8 @@ export class HomeComponent implements OnInit {
     this.dialog.open(templateRef);
   }
   onNewBoardSubmit(e: Event) {
-    this.taskListService.createNewBoard(this.newBoard).then(res => this.notificationService.message(res));
-    this.newBoard = '';
+    this.taskListService.createNewBoard(this.newBoardForm.value.newBoard).then(res => this.notificationService.message(res));
+    this.newBoardForm.value.newBoard = '';
     this.dialog.closeAll();
   }
   onChange(boardId: string) {
